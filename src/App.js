3a61ee69f9741a9c384ff7993f21a3b0363c7095
Marcopolo Ramos <mpolox@gmail.com>
2022-05-13 22:32:12 -0500

    first test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import logo from './logo.svg';
+import ReactECharts from "echarts-for-react";
 import './App.css';
 
 function App() {
+
+  const option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      left: 'center'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: '40',
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ]
+      }
+    ]
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my first SWA appication test !!
-        </p>
-      </header>
+          <ReactECharts option={option} />
+          <p>
+            Hello
+          </p>
     </div>
   );
 }
