64cd148378bc8f354da6f8a5a334e5a05e50355a
Marcopolo Ramos <mpolox@gmail.com>
2022-05-26 21:53:14 -0500

    update

--- src/components/dashboard/DashboardScreen.js
+++ src/components/dashboard/DashboardScreen.js
@@ -1,36 +1,33 @@
 import React from "react";
-import { Button, Card, Container } from "react-bootstrap";
-import "../../Styles/App.css"
-import ReactECharts from 'echarts-for-react';
-
+import { Button, Card, Col, Container, Row, Stack } from "react-bootstrap";
+import "../../Styles/App.css";
+import ReactECharts from "echarts-for-react";
 
 export const DashboardScreen = () => {
-
-  const getDashboardData = async() => {
+  const getDashboardData = async () => {
     console.log("Get Data");
     // const url = "https://localhost:7077/api/Clases/GetClases";
 
     // const response = await fetch(url);
     // const data = await response.json();
     // console.log(data);
-  }
+  };
 
   getDashboardData();
 
   const option = {
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      top: '5%',
-      left: 'center'
-    },
+    backgroundColor: '#2c343c',
     series: [
       {
         name: 'Access From',
         type: 'pie',
         radius: ['40%', '70%'],
         avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 5,
+          borderColor: '#2c343c',
+          borderWidth: 1
+        },      
         label: {
           show: false,
           position: 'center'
@@ -57,73 +54,99 @@ export const DashboardScreen = () => {
   };
 
   return (
-    <div className="home">
       <Container>
-      <br/>
-      <Card>
-        <Card.Header>Dashboard2</Card.Header>
-        <Card.Body>
-          <Card.Title>Card T33itle</Card.Title>
-          <Card.Text>
-            Some quick exampsdfsdfle text to 
-            build on the card title and 
-            make up the bulk of the sdf
-            card's content..ffff
-          </Card.Text>
-          <Button variant="primary">Go somewhere33</Button>
-        </Card.Body>
-      </Card>
-      <br/>  
-      <Card>
-        <Card.Header>Dashboard3332</Card.Header>
-        <Card.Body>
-          <Card.Title>Card T33itle</Card.Title>
-          <Card.Text>
-            Some quick example text to 
-            build on the card title and 
-            make up the bulk of the 
-            card's content..ffff
-          </Card.Text>
-          <Button variant="primary">Go somewhere223</Button>
-        </Card.Body>
-      </Card>
-      <br/>        
-      <br/>
-      <Card>
-        <Card.Header>Dashboard</Card.Header>
-        <Card.Body>
-          <ReactECharts option={option}/>
-        </Card.Body>
-      </Card>
-      <br/>
-      <Card>
-        <Card.Header>Dashboard2</Card.Header>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to 
-            build on the card title and 
-            make up the bulk of the 
-            card's content..ffff
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      <br/>      
-      <Card>
-        <Card.Header>Dashboard2</Card.Header>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to 
-            build on the card title and 
-            make up the bulk of the 
-            card's content..
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>         
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card T33itle</Card.Title>
+            <Card.Text>
+              Some quick exampsdfsdfle text to build on the card title and make
+              up the bulk of the sdf card's content..ffff
+            </Card.Text>
+            <Button variant="primary">Go somewhere33</Button>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card style={{ height: '28rem' }}>
+          <Card.Header>Dashboard</Card.Header>
+          <Card.Body>
+            <ReactECharts style={{height: "100%"}} option={option} />
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content..ffff
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content..
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content..
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content..
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content..
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>Dashboard2</Card.Header>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content..
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>    
       </Container>
-    </div>
   );
 };
