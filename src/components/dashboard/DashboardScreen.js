7e6bd7e150fbe3393582d75843629e5613697c9b
Marcopolo Ramos <mpolox@gmail.com>
2022-05-29 00:18:42 -0500

    update

--- src/components/dashboard/DashboardScreen.js
+++ src/components/dashboard/DashboardScreen.js
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, Col, Container, Row, Spinner, Stack } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Row,
+  Spinner,
+  Stack,
+} from "react-bootstrap";
 import "../../Styles/App.css";
 import ReactECharts from "echarts-for-react";
 
-
-
 export const DashboardScreen = () => {
   function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   const [data, setData] = useState("");
   const getDashboardData = async () => {
-     const url = "https://eu2apisisdev01.azurewebsites.net/api/operation/GetResumen";
-     const response = await fetch(url);
-     const resp = await response.json();
-     await sleep(3000);
-    setData(resp.data);    
+    const url =
+      "https://eu2apisisdev01.azurewebsites.net/api/operation/GetResumen";
+    const response = await fetch(url);
+    const resp = await response.json();
+    setData(resp.data);
   };
 
-  useEffect( () => {
-    getDashboardData()
+  useEffect(() => {
+    getDashboardData();
   }, []);
 
   const option3 = {
@@ -54,7 +60,7 @@ export const DashboardScreen = () => {
   const option2 = {
     tooltip: {
       trigger: "item",
-    },    
+    },
     xAxis: {
       type: "category",
       data: ["1er Parcial", "2o Parcial", "3er Parcial", "4o Parcial"],
@@ -71,12 +77,12 @@ export const DashboardScreen = () => {
         data: [25, 25, 20, 25],
         type: "line",
         smooth: true,
-        lineStyle: { 
+        lineStyle: {
           color: "#446A46",
-          width: 5
+          width: 5,
         },
-        areaStyle: {          
-          color: "#14C38E"
+        areaStyle: {
+          color: "#14C38E",
         },
       },
     ],
@@ -87,8 +93,8 @@ export const DashboardScreen = () => {
       trigger: "item",
     },
     legend: {
-      bottom: '5%'
-    },   
+      bottom: "5%",
+    },
     series: [
       {
         name: "Materia:",
@@ -124,80 +130,75 @@ export const DashboardScreen = () => {
       },
     ],
   };
- 
 
   const showContainer = () => {
     if (data) {
       console.log("Muestra dashboard");
       return (
         <Container>
-        <br />
-        <Card>
-          <Card.Header>Resumen</Card.Header>
-          <Card.Body>
-            <Card.Title>{data.alumno.nombre} {data.alumno.apellidos}, Matricula: {data.alumno.matricula}</Card.Title>
-            <Card.Text>
-              {data.descripcion} <br/>
-              {data.alumno.nota} <br/>
-              Materias cursadas: {data.materias.length}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br />
-  
-        <Card style={{ height: "28rem" }}>
-          <Card.Header>Creditos segun materia</Card.Header>
-          <Card.Body>
-            <ReactECharts style={{ height: "100%" }} option={option} />
-          </Card.Body>
-        </Card>
-        <br />
-        <Card style={{ height: "28rem" }}>
-          <Card.Header>Dashboard2</Card.Header>
-          <Card.Body>
-            <ReactECharts style={{ height: "100%" }} option={option2} />
-          </Card.Body>
-        </Card>
-        <br />
-        <Card style={{ height: "28rem" }}>
-          <Card.Header>Dashboard2</Card.Header>
-          <Card.Body>
-            <ReactECharts style={{ height: "100%" }} option={option3} />
-          </Card.Body>
-        </Card>
-        <br />
-        <Card>
-          <Card.Header>Dashboard2</Card.Header>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content..
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <br />
-      </Container>
-      )
+          <br />
+          <Card>
+            <Card.Header>Resumen</Card.Header>
+            <Card.Body>
+              <Card.Title>
+                {data.alumno.nombre} {data.alumno.apellidos}
+              </Card.Title>
+              <Card.Text>
+                Matricula:{data.alumno.matricula} <br />
+                {data.descripcion} <br />
+                {data.alumno.nota} <br />
+                Cursando {data.materias.length} materias
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+
+          <Card style={{ height: "28rem" }}>
+            <Card.Header>Creditos segun materia</Card.Header>
+            <Card.Body>
+              <ReactECharts style={{ height: "100%" }} option={option} />
+            </Card.Body>
+          </Card>
+          <br />
+          <Card style={{ height: "28rem" }}>
+            <Card.Header>Dashboard2</Card.Header>
+            <Card.Body>
+              <ReactECharts style={{ height: "100%" }} option={option2} />
+            </Card.Body>
+          </Card>
+          <br />
+          <Card style={{ height: "28rem" }}>
+            <Card.Header>Dashboard2</Card.Header>
+            <Card.Body>
+              <ReactECharts style={{ height: "100%" }} option={option3} />
+            </Card.Body>
+          </Card>
+          <br />
+          <Card>
+            <Card.Header>Dashboard2</Card.Header>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content..
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <br />
+        </Container>
+      );
     }
-    return(
+    return (
       <Container className="text-center">
-        <br/>
-        Espera un momento... 
-        <br/>
-        <br/>
+        <br />
+        Espera un momento...
+        <br />
+        <br />
         <Spinner animation="border" variant="primary" />
-
       </Container>
+    );
+  };
 
-    )
-
-  }
-
-  return (
-    <>
-    {showContainer()}
-    </>
-  );
+  return <>{showContainer()}</>;
 };
