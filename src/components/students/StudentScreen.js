42058e438c04f7fa5f37bc030bcd28167ca8e4dc
Marcopolo Ramos <mpolox@gmail.com>
2022-06-01 23:20:34 -0500

    update

--- src/components/students/StudentScreen.js
+++ src/components/students/StudentScreen.js
@@ -1,7 +1,14 @@
-import React  from "react";
-import { Accordion, Card, Container } from "react-bootstrap";
+import React, { useReducer }  from "react";
+import { Accordion, Button, Card, Container } from "react-bootstrap";
+import { ACTIONS } from "../../Reducers/actions";
+import { StudentReducer } from "../../Reducers/StudentReducer";
 
 export const StudentScreen = () => {
+  
+
+
+  const [state, dispatch] = useReducer(StudentReducer, {count: 0});
+
   return (
     <div>
       <Container>
@@ -10,8 +17,12 @@ export const StudentScreen = () => {
           <Accordion.Item eventKey="0">
             <Accordion.Header>Matemáticas</Accordion.Header>
             <Accordion.Body>
-            <Card style={{ height: "28rem" }}>
+            <Card style={{ height: "28rem" }}>              
               <Card.Body>
+                Result: {state.count} <br/>
+                <Button variant="success" onClick={() => dispatch({ type: ACTIONS.INCREMENT})}>+</Button>
+                <Button variant="danger" onClick={() => dispatch({ type: ACTIONS.DECREMENT})}>-</Button>
+
               </Card.Body>
               </Card>
             </Accordion.Body>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { LoginScreen } from '../components/login/login';
+import { ACTIONS } from '../Reducers/actions';
 import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
+  const r = ACTIONS.DECREMENT;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/*" element={<DashboardRoutes />} />
-      </Routes>    
+      </Routes>
+      <div>{r}</div>
     </BrowserRouter>
   )
 }
